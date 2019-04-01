34db6397f49a4fd1bcbabeb189fb863446d05f76
helloMJW <majianwei168@outlook.com>
2019-04-01 16:39:54 +0800

    每年入职人数总和

--- mysql/v1/list.sql
+++ mysql/v1/list.sql
@@ -19,3 +19,11 @@ select dept.emp_no, dep.dept_no, dep.dept_name, max(sal.salary) from departments
 select dept.emp_no, max(sal.salary), dept.dept_no from dept_emp as dept
 	left join salaries as sal on sal.emp_no = dept.emp_no
 	where dept.dept_no = 'd009'; # 条数-223644 最大额-144866
+
+
+# 每年入职人数  35316 共6年数据
+select count(*) as people, YEAR(hire_date) as entry_date from employees group by YEAR(hire_date) order by entry_date desc;
+# 指定年份间每年入职人数
+select count(*) as people, YEAR(hire_date) as entry_date from employees where hire_date > str_to_date('1992', '%Y-%m-%d') and hire_date <= str_to_date('1997', '%Y-%m-%d')  group by YEAR(hire_date) order by entry_date desc;
+# 数据验证
+select hire_date from employees where hire_date >= str_to_date('2000', '%Y-%m-%d') and hire_date < str_to_date('2001', '%Y-%m-%d');
