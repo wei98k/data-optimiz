5f878c9d2140ade93a6f20e877a3818335c0e37f
辣条拌鱼翅 <majianwei168@outlook.com>
2019-05-28 14:47:57 +0800

    最多工资员工

--- php/v1/index.php
+++ php/v1/index.php
@@ -22,6 +22,7 @@ include('./page.class.php');
             <th>first_name</th>
             <th>last_name</th>
             <th>职称</th>
+            <th>工资</th>
             <th>在职</th>
             <th>性别</th>
             <th>生日</th>
@@ -30,20 +31,23 @@ include('./page.class.php');
         <?php 
             // 所有员工
             $sql = "select 
-                    e.emp_no, birth_date, first_name, last_name, gender, hire_date, 
+                    e.emp_no, birth_date, first_name, last_name, gender, hire_date, max(s.salary) as salary,
                     (select title from titles as t where t.emp_no = e.emp_no order by to_date desc limit 1) as title,
                     (select to_date from titles as t where t.emp_no = e.emp_no order by to_date desc limit 1) as worktime
                     from employees as e";
             // $sql .= " where e.last_name like '%Rem%'";
+            $sql .= " left join salaries s on e.emp_no = s.emp_no";
+            // $sql .= " having max(s.salary)";
             $sql .= " order by e.birth_date";
-            $sql .= " limit 20";
-            // echo $sql;
+            $sql .= " limit 50";
+            echo $sql;
             foreach($dbh->query($sql) as $row) {
                 echo '<tr>';
                 echo '<td><a href="./detail.php?empno='.$row ['emp_no'].'"> '. $row ['emp_no'] .'</a></td>';
                 echo '<td>'. $row ['first_name'] .'</td>';
                 echo '<td>'. $row ['last_name'] .'</td>';
                 echo '<td>'. $row ['title'] .'</td>';
+                echo '<td>'. $row ['salary'] .'</td>';
                 if($row ['worktime'] == '9999-01-01') {
                     echo '<td>'. '是' .'</td>';
                 } else {
